97930f43fdabb0ca85a247e06d8f19e9e7c7ace5
H.N <hayato.n.fl@gmail.com>
2024-06-18 10:58:12 +0900

    Fix more.

--- src/calculator.rs
+++ src/calculator.rs
@@ -17,3 +17,46 @@ impl Calculator {
         self.subend1 - self.subend2
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rust_decimal::Decimal;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_integer_calculation() {
+        let decimal1 = Decimal::from_str("5").unwrap();
+        let decimal2 = Decimal::from_str("3").unwrap();
+        let calculator = Calculator::new(decimal1, decimal2);
+
+        assert_eq!(Decimal::from_str("2").unwrap(), calculator.calc());
+    }
+
+    #[test]
+    fn test_float_calculation() {
+        let decimal1 = Decimal::from_str("5.7").unwrap();
+        let decimal2 = Decimal::from_str("3.5").unwrap();
+        let calculator = Calculator::new(decimal1, decimal2);
+
+        assert_eq!(Decimal::from_str("2.2").unwrap(), calculator.calc());
+    }
+
+    #[test]
+    fn test_negative_integer_result() {
+        let decimal1 = Decimal::from_str("3").unwrap();
+        let decimal2 = Decimal::from_str("5").unwrap();
+        let calculator = Calculator::new(decimal1, decimal2);
+
+        assert_eq!(Decimal::from_str("-2").unwrap(), calculator.calc());
+    }
+
+    #[test]
+    fn test_negative_float_result() {
+        let decimal1 = Decimal::from_str("3.7").unwrap();
+        let decimal2 = Decimal::from_str("5.5").unwrap();
+        let calculator = Calculator::new(decimal1, decimal2);
+
+        assert_eq!(Decimal::from_str("-1.8").unwrap(), calculator.calc());
+    }
+}
